157765d8f2ada312f6f9bd92062fe85ac1028cf4
VasuVibingPrivetly <143987026+vasuvibingprivetly@users.noreply.github.com>
2024-02-12 08:19:07 +0530

    calender.dart

--- lib/utils/calender.dart
+++ lib/utils/calender.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
-  const CalendarPage({Key? key}) : super(key: key);
+  const CalendarPage({super.key});
 
   @override
   _CalendarPageState createState() => _CalendarPageState();
